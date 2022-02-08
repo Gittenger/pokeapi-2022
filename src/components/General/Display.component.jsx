60befc04e884fad70ef9415116d78ba602963552
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-08 03:18:05 -0500

    basic filter working

--- src/components/General/Display.component.jsx
+++ src/components/General/Display.component.jsx
@@ -42,26 +42,36 @@ const Display = ({ id: currentPage }) => {
             activePage={activePage}
           />
           <div className="grid gap-y-14 gap-x-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {partialData.map((el, i) =>
-              isIndexInBounds(offset, pageLimit, i) ? (
-                <div
-                  className="flex flex-col justify-start items-center p-5 bg-slate-200 ring-4 ring-gray-800 shadow-2xl rounded-3xl"
-                  key={i}
-                >
-                  <h3 className="capitalize text-gray-800 text-2xl underline font-bold">
-                    <Link to={`/pokemon/${el.name}`}>{el.name}</Link>
-                  </h3>
-                  <div className="mt-2">
-                    <img
-                      src={el.sprites.other['official-artwork'].front_default}
-                      alt=""
-                    />
+            {partialData
+              .map((el) => {
+                return {
+                  ...el,
+                  types: el.types.filter((type) => {
+                    return type.type.name == 'grass'
+                  }),
+                }
+              })
+              .filter((el) => el.types.length > 0)
+              .map((el, i) => {
+                return isIndexInBounds(offset, pageLimit, i) ? (
+                  <div
+                    className="flex flex-col justify-start items-center p-5 bg-slate-200 ring-4 ring-gray-800 shadow-2xl rounded-3xl"
+                    key={i}
+                  >
+                    <h3 className="capitalize text-gray-800 text-2xl underline font-bold">
+                      <Link to={`/pokemon/${el.name}`}>{el.name}</Link>
+                    </h3>
+                    <div className="mt-2">
+                      <img
+                        src={el.sprites.other['official-artwork'].front_default}
+                        alt=""
+                      />
+                    </div>
                   </div>
-                </div>
-              ) : (
-                ''
-              )
-            )}
+                ) : (
+                  ''
+                )
+              })}
           </div>
           <Pagination
             className="mt-7"
