6f275fc65699bdab437ae3a2525884e135dfccce
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-09 06:49:34 -0500

    rm unneeded reducers

--- src/reducer/actions.js
+++ src/reducer/actions.js
@@ -1,6 +1,4 @@
 export const SET_DATA = 'SET_DATA'
-export const SET_URLS = 'SET_URLS'
 export const SET_POKEMON_DATA = 'SET_POKEMON_DATA'
-export const SET_ABILITIES_DATA = 'SET_ABILITIES_DATA'
 
-export default { SET_DATA, SET_URLS, SET_POKEMON_DATA, SET_ABILITIES_DATA }
+export default { SET_DATA, SET_POKEMON_DATA }
