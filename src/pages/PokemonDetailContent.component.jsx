7e918a911e82f4d54add94add47f191269b287c2
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-11 07:55:49 -0500

    guards for undef obj in movesRender

--- src/pages/PokemonDetailContent.component.jsx
+++ src/pages/PokemonDetailContent.component.jsx
@@ -38,15 +38,15 @@ const MovesRender = React.memo(function ({ moves, movesObject }) {
               <p className="underline">{el.name}</p>
               <p>
                 effect:{' '}
-                {movesObject[el.url]?.effect_entries.short_effect.replace(
+                {movesObject[el.url]?.effect_entries?.short_effect?.replace(
                   '$effect_chance%',
                   `${movesObject[el.url]?.effect_chance}%`
                 )}
               </p>
-              <p>damage-class: {movesObject[el.url]?.damage_class.name}</p>
+              <p>damage-class: {movesObject[el.url]?.damage_class?.name}</p>
               <p>
                 "
-                {movesObject[el.url]?.flavor_text_entries.text
+                {movesObject[el.url]?.flavor_text_entries?.text
                   .replace('\f', '\n')
                   .replace('\u00ad\n', '')
                   .replace('\u00ad', '')
