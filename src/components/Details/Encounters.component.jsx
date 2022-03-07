e1f2810c03fb768c3ce03f50f1e0054ad234c23a
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-07 18:55:32 -0500

    basic encounters grid okay

--- src/components/Details/Encounters.component.jsx
+++ src/components/Details/Encounters.component.jsx
@@ -1,39 +1,47 @@
 import React from 'react'
 
+import styles from './styles/Encounters.module.css'
+
 import {
   transformAreaString,
   checkVersionEncounters,
 } from '../../utils/utilFunctions.js'
 
 export const Encounters = React.memo(({ encountersData, versionsMap }) => (
-  <div>
+  <div className="w-5/6 flex flex-col items-center">
     <h2 className="text-2xl font-bold underline">LOCATIONS</h2>
+    <p className="underline self-start ml-8">game-versions:</p>
     {encountersData.length > 0 ? (
-      <ul>
-        <p className="underline">game-versions:</p>
+      <ul className={`${styles.grid} mt-10`}>
         {versionsMap.map((thisVersion, versionIndex) => {
           return (
             <li key={versionIndex}>
               <p className="font-bold text-xl underline">{thisVersion.name}</p>
-              {
-                // check if this version has some encounter matching current version
-                // if so, return them filtered out below
-                encountersData.some(checkVersionEncounters(thisVersion)) ? (
-                  encountersData
-                    .filter(checkVersionEncounters(thisVersion))
-                    // transform encounter location strings for presentation
-                    ?.map(({ location_area }) =>
-                      transformAreaString(location_area.name)
-                    )
-                    // filter remaining duplicates, then display
-                    .filter((el, i, arr) => {
-                      return arr.indexOf(el) == i
-                    })
-                    .map((el, i) => <p key={i}>{el}</p>)
-                ) : (
-                  <p>NONE</p>
-                )
-              }
+              <ul>
+                {
+                  // check if this version has some encounter matching current version
+                  // if so, return them filtered out below
+                  encountersData.some(checkVersionEncounters(thisVersion)) ? (
+                    encountersData
+                      .filter(checkVersionEncounters(thisVersion))
+                      // transform encounter location strings for presentation
+                      ?.map(({ location_area }) =>
+                        transformAreaString(location_area.name)
+                      )
+                      // filter remaining duplicates, then display
+                      .filter((el, i, arr) => {
+                        return arr.indexOf(el) == i
+                      })
+                      .map((el, i) => (
+                        <li key={i}>
+                          <p>{el}</p>
+                        </li>
+                      ))
+                  ) : (
+                    <p>NONE</p>
+                  )
+                }
+              </ul>
             </li>
           )
         })}
