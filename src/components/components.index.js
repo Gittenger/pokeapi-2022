3413105ca6221d2cb17a21bd3271052fb231274f
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-08 05:09:56 -0500

    mv display/layout

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -4,8 +4,6 @@ import Encounters from './Details/Encounters.component.jsx'
 import Graph from './Details/Graph.component.jsx'
 import Items from './Details/Items.component.jsx'
 import Moves from './Details/Moves.component.jsx'
-import Display from './General/Display.component.jsx'
-import Layout from './General/Layout.component.jsx'
 import Navbar from './General/Navbar.component.jsx'
 import Pagination from './General/Pagination.component.jsx'
 import Card from './Home/Card.component.jsx'
@@ -18,8 +16,6 @@ export const CIndex = {
   Graph,
   Items,
   Moves,
-  Display,
-  Layout,
   Navbar,
   Pagination,
   Card,
