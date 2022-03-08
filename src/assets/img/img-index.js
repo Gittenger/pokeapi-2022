cb17f87de0ad779e96293e55206b8ca4c06bdce7
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-08 01:11:53 -0500

    logo imgs

--- src/assets/img/img-index.js
+++ src/assets/img/img-index.js
@@ -41,6 +41,29 @@ import SteelCard from './img/steel.png'
 import UnknownCard from './img/unknown.png'
 import WaterCard from './img/water.png'
 
+import RedLogo from './img/logos/red.png'
+import BlackLogo from './img/logos/black.png'
+import WhiteLogo from './img/logos/white.png'
+import BlueLogo from './img/logos/blue.png'
+import ColosseumLogo from './img/logos/colosseum.png'
+import CrystalLogo from './img/logos/crystal.png'
+import DiamondLogo from './img/logos/diamond.png'
+import EmeraldLogo from './img/logos/emerald.png'
+import FireredLogo from './img/logos/firered.png'
+import GoldLogo from './img/logos/gold.png'
+import HeartgoldLogo from './img/logos/heartgold.png'
+import LeafgreenLogo from './img/logos/leafgreen.png'
+import PearlLogo from './img/logos/pearl.png'
+import PlatinumLogo from './img/logos/platinum.png'
+import RubyLogo from './img/logos/ruby.png'
+import SapphireLogo from './img/logos/sapphire.png'
+import SilverLogo from './img/logos/silver.png'
+import SoulsilverLogo from './img/logos/soulsilver.png'
+import XLogo from './img/logos/x.png'
+import XDLogo from './img/logos/xd.png'
+import YLogo from './img/logos/y.png'
+import YellowLogo from './img/logos/yellow.png'
+
 export const images = {
   img: {
     PokeLogo,
@@ -64,6 +87,30 @@ export const images = {
     SteelCard,
     UnknownCard,
     WaterCard,
+    logos: {
+      RedLogo,
+      BlueLogo,
+      ColosseumLogo,
+      CrystalLogo,
+      DiamondLogo,
+      EmeraldLogo,
+      FireredLogo,
+      GoldLogo,
+      HeartgoldLogo,
+      LeafgreenLogo,
+      PearlLogo,
+      PlatinumLogo,
+      RubyLogo,
+      SapphireLogo,
+      SilverLogo,
+      SoulsilverLogo,
+      XLogo,
+      XDLogo,
+      YLogo,
+      YellowLogo,
+      BlackLogo,
+      WhiteLogo,
+    },
   },
   icons: {
     BugIcon,
