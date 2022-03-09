d77acf8bd4f792d50d9213c6a8157c372f663205
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-09 18:46:06 -0500

    calculateCount func, refactor/clean up

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -5,51 +5,45 @@ import MainContext from '../contexts/MainContext'
 
 import { usePagination, useTitle } from '../utils/hooks'
 import { usePokemonData } from '../utils/dataHooks'
-import { isIndexInBounds } from '../utils/utilFunctions.js'
+import { isIndexInBounds, calculateCount } from '../utils/utilFunctions.js'
 import CIndex from '../components/components.index.js'
 
 const Home = ({ title }) => {
+  // simple hooks
   const { currentPage } = useParams()
   let navigate = useNavigate()
   useTitle(title)
-  // page settings
+
+  // pagination hooks
   const { pageLimit, activePageNumber, setActivePageNumber } =
     useContext(MainContext)
   const { pageCount, offset } = usePagination(currentPage)
   const [filteredPageCount, setFilteredPageCount] = useState(0)
 
-  // get pokemon data
+  // pokemon data hook
   const [pokemonObject, urlsMap, dataProcessed] = usePokemonData()
 
+  // filter state
   const [filterType, setFilterType] = useState('none')
 
   useEffect(() => {
-    // console.log(currentPage)
-    if (currentPage) {
-      setActivePageNumber(currentPage)
-      localStorage.setItem('activePageNumber', currentPage)
-    } else {
-      setActivePageNumber('1')
-      localStorage.setItem('activePageNumber', '1')
-    }
+    // set active page from url param
+    setActivePageNumber(currentPage)
+    localStorage.setItem('activePageNumber', currentPage)
   }, [dataProcessed, currentPage])
 
   const handleFilter = (e) => {
     const targetType = e.target.attributes.value.value
     setFilterType(targetType)
-    const filteredLength = urlsMap.filter((url) => {
-      // setFilterActive(filterType)
-      return pokemonObject[url.url]?.types.some(
-        (type) => type.name == targetType
-      )
-    }).length
+    const filteredLength = urlsMap.filter((url) =>
+      pokemonObject[url.url]?.types.some((type) => type.name == targetType)
+    ).length
 
     navigate('/1', {
       replace: true,
     })
 
-    let count = Math.ceil(parseInt(filteredLength) / pageLimit)
-    setFilteredPageCount(count)
+    setFilteredPageCount(calculateCount(filteredLength, pageLimit))
   }
 
   const { Pagination, RenderFromType, Dropdown } = CIndex
@@ -140,16 +134,15 @@ const Home = ({ title }) => {
           />
           <div className="w-full grid gap-y-14 gap-x-5 place-content-center place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4">
             {urlsMap
-              .filter((url) => {
-                if (filterType !== 'none') {
-                  return pokemonObject[url.url]?.types.some(
-                    (type) => type.name == filterType
-                  )
-                } else return true
-              })
-              .map((url, i, arr) => {
-                console.log(pokemonObject[url.url])
-                return isIndexInBounds(offset, pageLimit, i) ? (
+              .filter((url) =>
+                filterType == 'none'
+                  ? true
+                  : pokemonObject[url.url]?.types.some(
+                      (type) => type.name == filterType
+                    )
+              )
+              .map((url, i) =>
+                isIndexInBounds(offset, pageLimit, i) ? (
                   <RenderFromType
                     render="card"
                     name={pokemonObject[url.url]?.name}
@@ -157,10 +150,8 @@ const Home = ({ title }) => {
                     types={pokemonObject[url.url]?.types}
                     key={i}
                   />
-                ) : (
-                  ''
-                )
-              })}
+                ) : null
+              )}
           </div>
           <Pagination
             className="mt-7"
