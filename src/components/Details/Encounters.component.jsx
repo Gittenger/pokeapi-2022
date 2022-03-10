71b232855b847be8225d4d37b7d2820a3b0ff571
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-10 02:18:26 -0500

    placeholder msg for no location pokemon

--- src/components/Details/Encounters.component.jsx
+++ src/components/Details/Encounters.component.jsx
@@ -11,57 +11,58 @@ import {
 export const Encounters = React.memo(({ encountersData, versionsMap }) => (
   <div className="w-full flex flex-col items-center">
     <h2 className="text-2xl font-bold underline">LOCATIONS</h2>
-    <p className="underline self-start ml-8">game-versions:</p>
     {encountersData.length > 0 ? (
-      <ul
-        className={`${styles.grid} mt-10 w-[90%] md:w-[80%] lg:w-[65%] xl:w-[50%]`}
-      >
-        {versionsMap.map((thisVersion, versionIndex) => {
-          return (
-            <li key={versionIndex}>
-              <div className="font-bold text-xl underline rounded-lg bg-[#796921] ring-inset ring-red-700 ring-4">
-                <div
-                  className={`absolute w-56 h-44 bg-zinc-900 rounded-2xl z-0`}
-                ></div>
-                <div
-                  className={`${styles.imgBg} absolute w-56 h-44 rounded-2xl z-10 shadow-2xl`}
-                ></div>
-                <img
-                  className="z-30 w-[90%]"
-                  src={getLogo(thisVersion.name)}
-                  alt=""
-                />
-              </div>
-              {
-                <div className="relative w-full h-full bg-[#000] p-3 rounded-2xl">
-                  <div className="absolute w-[97%] h-[90%] bg-zinc-900 z-0 rounded-xl"></div>
-                  <p className="italic text-xl min-w-full min-h-full text-whit px-5 py-7 z-30">
-                    {
-                      // check if this version has some encounter matching current version
-                      // if so, return them filtered out below
-                      encountersData.some(checkVersionEncounters(thisVersion))
-                        ? encountersData
-                            .filter(checkVersionEncounters(thisVersion))
-                            // transform encounter location strings for presentation
-                            ?.map(({ location_area }) =>
-                              transformAreaString(location_area.name)
-                            )
-                            // filter remaining duplicates, then display
-                            .filter((el, i, arr) => {
-                              return arr.indexOf(el) == i
-                            })
-                            .join(', ')
-                        : 'None...'
-                    }
-                  </p>
+      <>
+        <ul
+          className={`${styles.grid} mt-4 w-[90%] md:w-[80%] lg:w-[65%] xl:w-[50%]`}
+        >
+          {versionsMap.map((thisVersion, versionIndex) => {
+            return (
+              <li key={versionIndex}>
+                <div className="font-bold text-xl underline rounded-lg bg-[#796921] ring-inset ring-red-700 ring-4">
+                  <div
+                    className={`absolute w-56 h-44 bg-zinc-900 rounded-2xl z-0`}
+                  ></div>
+                  <div
+                    className={`${styles.imgBg} absolute w-56 h-44 rounded-2xl z-10 shadow-2xl`}
+                  ></div>
+                  <img
+                    className="z-30 w-[90%]"
+                    src={getLogo(thisVersion.name)}
+                    alt=""
+                  />
                 </div>
-              }
-            </li>
-          )
-        })}
-      </ul>
+                {
+                  <div className="relative w-full h-full bg-[#000] p-3 rounded-2xl">
+                    <div className="absolute w-[97%] h-[90%] bg-zinc-900 z-0 rounded-xl"></div>
+                    <p className="italic text-xl min-w-full min-h-full text-whit px-5 py-7 z-30">
+                      {
+                        // check if this version has some encounter matching current version
+                        // if so, return them filtered out below
+                        encountersData.some(checkVersionEncounters(thisVersion))
+                          ? encountersData
+                              .filter(checkVersionEncounters(thisVersion))
+                              // transform encounter location strings for presentation
+                              ?.map(({ location_area }) =>
+                                transformAreaString(location_area.name)
+                              )
+                              // filter remaining duplicates, then display
+                              .filter((el, i, arr) => {
+                                return arr.indexOf(el) == i
+                              })
+                              .join(', ')
+                          : 'None...'
+                      }
+                    </p>
+                  </div>
+                }
+              </li>
+            )
+          })}
+        </ul>
+      </>
     ) : (
-      'none'
+      <p>This Pokémon is not encountered in the wild....</p>
     )}
   </div>
 ))
