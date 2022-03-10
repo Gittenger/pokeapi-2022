8ea2e8fa0983f4a86e817dbd38442fc7620beca2
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-09 23:52:23 -0500

    change label

--- src/components/General/Dropdown.component.jsx
+++ src/components/General/Dropdown.component.jsx
@@ -41,7 +41,7 @@ export default function DropdownWithOpts({ type, ...props }) {
     />
   ) : type === 'filter' ? (
     <DropdownComponent
-      display="Filter By:"
+      display="Filter Type:"
       options={dropdownData.filterDropdownOpt}
       {...props}
     />
