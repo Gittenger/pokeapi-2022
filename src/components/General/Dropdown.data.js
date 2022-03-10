5d341ec221559903617a3245f60ce34460fcd7ba
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-09 19:07:29 -0500

    dropdown options data

---
+++ src/components/General/Dropdown.data.js
@@ -0,0 +1,95 @@
+export const filterDropdownOpt = [
+  {
+    value: 'none',
+    display: 'Remove filter',
+  },
+  {
+    value: 'grass',
+    display: 'Grass',
+  },
+  {
+    value: 'poison',
+    display: 'Poison',
+  },
+  {
+    value: 'fire',
+    display: 'Fire',
+  },
+  {
+    value: 'water',
+    display: 'Water',
+  },
+  {
+    value: 'bug',
+    display: 'Bug',
+  },
+  {
+    value: 'electric',
+    display: 'Electric',
+  },
+  {
+    value: 'ground',
+    display: 'Ground',
+  },
+  {
+    value: 'normal',
+    display: 'Normal',
+  },
+  {
+    value: 'flying',
+    display: 'Flying',
+  },
+  {
+    value: 'fighting',
+    display: 'Fighting',
+  },
+  {
+    value: 'psychic',
+    display: 'Psychic',
+  },
+  {
+    value: 'rock',
+    display: 'Rock',
+  },
+  {
+    value: 'fairy',
+    display: 'Fairy',
+  },
+  {
+    value: 'dragon',
+    display: 'Dragon',
+  },
+  {
+    value: 'ice',
+    display: 'Ice',
+  },
+]
+
+export const imageDropdownOpt = [
+  {
+    value: 'main',
+    display: 'Main',
+  },
+
+  {
+    value: 'dream-world',
+    display: 'Dream World',
+  },
+
+  {
+    value: 'home',
+    display: 'Home',
+  },
+
+  {
+    value: 'animated',
+    display: 'Animated',
+  },
+]
+
+const dropdownData = {
+  filterDropdownOpt,
+  imageDropdownOpt,
+}
+
+export default dropdownData
