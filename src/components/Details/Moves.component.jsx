7d1317f7f639e6613cb5d13023529bf055932f63
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-10 21:28:55 -0500

    attribution for regex snippet

--- src/components/Details/Moves.component.jsx
+++ src/components/Details/Moves.component.jsx
@@ -19,6 +19,12 @@ const Moves = React.memo(({ moves, movesObject }) => (
             <p>
               "
               {movesObject[el.url]?.flavor_text_entries?.text
+                // thanks to Naramsim on github for snippet
+                // Page breaks are treated just like newlines.
+                // Soft hyphens followed by newlines vanish.
+                // Letter-hyphen-newline becomes letter-hyphen, to preserve real
+                // hyphenation.
+                // Any other newline becomes a space.
                 .replace('\f', '\n')
                 .replace('\u00ad\n', '')
                 .replace('\u00ad', '')
