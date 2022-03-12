1230c1ea8095de34e9f8b9ff51f2dae83f075382
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-12 05:23:35 -0500

    border for bottom

--- src/components/Details/Abilities.component.jsx
+++ src/components/Details/Abilities.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Abilities = React.memo(({ abilities, abilitiesObject, className }) => (
+  <div className={`${className} pb-8 border-b border-white`}>
     <h2 className="font-bold italic underline mb-3 text-2xl">Abilities</h2>
     <ul className="space-y-5">
       {abilities.map((el, i) => {
