68ad0957fc2b5ab8ff7bf583beb829cc68fd7dfe
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-12 05:23:15 -0500

    improve font styling

--- src/components/Details/Abilities.component.jsx
+++ src/components/Details/Abilities.component.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Abilities = React.memo(({ abilities, abilitiesObject, className }) => (
     <h2 className="font-bold italic underline mb-3 text-2xl">Abilities</h2>
+    <ul className="space-y-5">
       {abilities.map((el, i) => {
         return (
           <li key={i}>
-            <p className="underline">{el.name}</p>
+            <p className="capitalize font-pokemon tracking-widest text-xl">
+              {el.name.replace(/-/g, ' ')}
+            </p>
             <p>{abilitiesObject[el.url]?.effect_entries.short_effect}</p>
           </li>
         )
