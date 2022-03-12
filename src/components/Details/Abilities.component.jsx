7a410be312575b57c4e3745a5e8acd3702707f8f
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-12 15:19:55 -0500

    pokemon font, spacing, sky color

--- src/components/Details/Abilities.component.jsx
+++ src/components/Details/Abilities.component.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Abilities = React.memo(({ abilities, abilitiesObject, className }) => (
   <div className={`${className} pb-8 border-b border-white`}>
     <h2 className="text-heading">Abilities</h2>
+    <ul className="space-y-8 mt-6">
       {abilities.map((el, i) => {
         return (
           <li key={i}>
-            <p className="capitalize font-pokemon tracking-widest text-xl">
+            <p className="capitalize font-pokemon tracking-widest text-3xl mb-2 text-sky-100">
               {el.name.replace(/-/g, ' ')}
             </p>
             <p>{abilitiesObject[el.url]?.effect_entries.short_effect}</p>
