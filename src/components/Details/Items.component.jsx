f7a768410d3562feedec812d17462e70e6b6cc32
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-12 04:24:39 -0500

    display img OK

--- src/components/Details/Items.component.jsx
+++ src/components/Details/Items.component.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import ImageObject from './ItemImgs.js'
 
-const Items = React.memo(({ held_items, itemsObject }) => (
-  <div className="mt-4">
-    <h2 className="underline">ITEMS</h2>
-    {held_items?.length === 0 ? (
-      <p>NONE</p>
-    ) : (
-      <ul>
-        {held_items.map((el, i) => (
-          <li key={i}>
-            <p className="underline">{el.name}</p>
-            <p>{itemsObject[el.url]?.effect_entries.short_effect}</p>
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
-))
+const Items = React.memo(({ held_items, itemsObject }) => {
+  return (
+    <div className="mt-4">
+      <h2 className="underline">ITEMS</h2>
+      {held_items?.length === 0 ? (
+        <p>NONE</p>
+      ) : (
+        <ul>
+          {held_items.map((el, i) => (
+            <li key={i}>
+              <p className="underline">{el.name}</p>
+              <img className="w-10" src={ImageObject[el.name]} alt="" />
+              <p>{itemsObject[el.url]?.effect_entries.short_effect}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+})
 
 export default Items
