19fb236330d5aa3d48b7f9c64b67646a04b6967f
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-14 20:46:01 -0400

    rm material-TW

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App'
 import './index.css'
 import './styles/animista.css'
 import './styles/myAnimations.css'
-import '@material-tailwind/react/tailwind.css'
 import MainProvider from './contexts/MainProvider.jsx'
 
 ReactDOM.render(
