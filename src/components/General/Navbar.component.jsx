d0abe503eee5cd8c5a45c65ba1ebb6a966dab3be
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-14 20:48:51 -0400

    rm material navbar

--- src/components/General/Navbar.component.jsx
+++ src/components/General/Navbar.component.jsx
@@ -2,18 +2,6 @@ import React, { useState, useContext } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import MainContext from '../../contexts/MainContext'
 
-import Navbar from '@material-tailwind/react/Navbar'
-import NavbarContainer from '@material-tailwind/react/NavbarContainer'
-import NavbarWrapper from '@material-tailwind/react/NavbarWrapper'
-import NavbarBrand from '@material-tailwind/react/NavbarBrand'
-import NavbarCollapse from '@material-tailwind/react/NavbarCollapse'
-import Nav from '@material-tailwind/react/Nav'
-import NavbarInput from '@material-tailwind/react/NavbarInput'
-// import NavbarToggler from '@material-tailwind/react/NavbarToggler'
-// import NavItem from '@material-tailwind/react/NavItem'
-// import NavLink from '@material-tailwind/react/NavLink'
-// import Icon from '@material-tailwind/react/Icon'
-
 import { usePokemonData } from '../../utils/dataHooks.js'
 import { calculateCount } from '../../utils/utilFunctions'
 
@@ -62,44 +50,10 @@ export default function NavBar() {
   }
 
   return (
-    <Navbar className={`${styles.navBar}`} color="teal" navbar>
-      <NavbarContainer>
-        <NavbarWrapper>
-          <NavbarBrand>
-            <Link to="/">
-              <img className="w-52" src={PokeLogo} alt="" />
-            </Link>
-          </NavbarBrand>
-          {/* <NavbarToggler
-            className={`${styles.navBtn}`}
-            onClick={() => setOpenNavbar(!openNavbar)}
-            ripple="dark"
-          /> */}
-        </NavbarWrapper>
-
-        <NavbarCollapse open={openNavbar}>
-          <Nav className={`${styles.navList}`}>
-            {/* <NavItem active="light" ripple="dark">
-              <Icon color="black" name="language" size="xl" />
-              Discover
-            </NavItem>
-            <NavLink href="#navbar" ripple="dark">
-              <Icon name="account_circle" size="xl" />
-              Profile
-            </NavLink>
-            <NavItem ripple="dark">
-              <Icon name="settings" size="xl" />
-              Settings
-            </NavItem> */}
-            <NavbarInput
-              value={searchPageCount === null ? '' : searchQuery}
-              type="text"
-              placeholder="Search here"
-              onChange={handleSearch}
-            />
-          </Nav>
-        </NavbarCollapse>
-      </NavbarContainer>
-    </Navbar>
+    <nav className="flex justify-center items-center py-3 bg-teal-500">
+      <Link to="/">
+        <img className="w-52" src={PokeLogo} alt="" />
+      </Link>
+    </nav>
   )
 }
