61aa5b43856d2f9a2679e725cd5a53f46f19c4f7
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-14 20:47:14 -0400

    custom button

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -4,6 +4,7 @@ import Encounters from './Details/Encounters.component.jsx'
 import Graph from './Details/Graph.component.jsx'
 import Items from './Details/Items.component.jsx'
 import Moves from './Details/Moves.component.jsx'
+import Button from './General/Button.component.jsx'
 import Navbar from './General/Navbar.component.jsx'
 import Pagination from './General/Pagination.component.jsx'
 import Card from './Home/Card.component.jsx'
@@ -16,6 +17,7 @@ export const CIndex = {
   Graph,
   Items,
   Moves,
+  Button,
   Navbar,
   Pagination,
   Card,
