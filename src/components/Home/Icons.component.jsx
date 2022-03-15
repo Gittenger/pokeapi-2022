37602ce874c989ea6052eeb90ac842ac26dd902c
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-15 04:52:39 -0400

    tweak sizing

--- src/components/Home/Icons.component.jsx
+++ src/components/Home/Icons.component.jsx
@@ -6,7 +6,7 @@ const Icons = ({ typeObject, types, className }) => {
       className={`${className} w-full flex justify-start items-center space-x-2`}
     >
       {types?.map((type, i) => (
-        <li className="md:w-20 w-16" key={i}>
+        <li className="lg:w-20 md:w-16 w-14" key={i}>
           <img src={typeObject ? typeObject[type.name].icon : ''} alt="" />
         </li>
       ))}
