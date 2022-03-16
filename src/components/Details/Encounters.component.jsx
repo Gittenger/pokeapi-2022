b063afc842a857c71544483bce5b5eaf2d3fe74b
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-16 03:20:14 -0400

    mq for encounters OK

--- src/components/Details/Encounters.component.jsx
+++ src/components/Details/Encounters.component.jsx
@@ -14,28 +14,27 @@ export const Encounters = React.memo(({ encountersData, versionsMap }) => (
     {encountersData.length > 0 ? (
       <>
         <ul
-          className={`${styles.grid} mt-4 w-[90%] md:w-[80%] lg:w-[65%] xl:w-[50%]`}
+          className={`${styles.grid} mt-4 w-[90%] xs:w-[70%] sm:w-[60%] md:w-[80%] lg:w-[65%] xl:w-[50%]`}
         >
           {versionsMap.map((thisVersion, versionIndex) => {
             return (
               <li key={versionIndex}>
                 <div className="font-bold text-xl underline rounded-lg bg-[#dfcc62] ring-inset ring-red-600 ring-4 relative">
                   <div
-                    className={`absolute w-56 h-44 bg-zinc-900 rounded-2xl z-0`}
+                    className={`absolute w-44 h-32 sm:w-56 sm:h:-44 bg-zinc-900 rounded-2xl z-0`}
                   ></div>
                   <div
-                    className={`${styles.imgBg} absolute w-56 h-44 rounded-2xl z-10 shadow-2xl`}
+                    className={`${styles.imgBg} absolute w-44 h-32 sm:w-56 sm:h:-44 rounded-2xl z-10 shadow-2xl`}
                   ></div>
                   <img
-                    className="z-30 w-[90%]"
+                    className="z-30 h-[120px] xs:h-auto xs:w-[65%] sm:w-[75%] md:w-[90%]"
                     src={getLogo(thisVersion.name)}
                     alt=""
                   />
                 </div>
                 {
-                  <div className="relative w-full h-full bg-[#000] p-3 rounded-2xl">
-                    <div className="absolute w-[97%] h-[90%] bg-zinc-800 z-0 rounded-xl"></div>
-                    <p className="italic text-xl min-w-full min-h-full text-whit px-5 py-7 z-30">
+                  <div className="relative w-full h-full bg-zinc-800  ring-inset ring-8 ring-black rounded-2xl">
+                    <p className="italic text-xl min-w-full min-h-full text-whit px-5 py-10 z-30">
                       {
                         // check if this version has some encounter matching current version
                         // if so, return them filtered out below
