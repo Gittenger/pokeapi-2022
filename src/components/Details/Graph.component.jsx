79caa915e721d0d65e60944db95204902e3251fc
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-16 02:05:50 -0400

    responsive stylings for graph OK

--- src/components/Details/Graph.component.jsx
+++ src/components/Details/Graph.component.jsx
@@ -8,18 +8,43 @@ const Graph = React.memo(({ stats }) => {
 
   useEffect(() => {
     if (stats.length > 0) {
-      var margin = { top: 30, right: 30, bottom: 180, left: 60 },
-        width = 500 - margin.left - margin.right,
-        height = 530 - margin.top - margin.bottom
+      const responsiveFn = (svg) => {
+        const container = d3.select(svg.node().parentNode),
+          width = parseInt(svg.style('width'), 10),
+          height = parseInt(svg.style('height'), 10),
+          aspect = width / height
 
-      var svg = d3
+        svg
+          .attr('viewBox', `0 0 ${width} ${height}`)
+          .attr('preserveAspectRatio', 'xMinYMid')
+          .call(resize)
+
+        d3.select(window).on('resize.' + container.attr('id'), resize)
+
+        function resize() {
+          const targetWidth = parseInt(container.style('width'))
+          svg.attr('width', targetWidth)
+          svg.attr('height', Math.round(targetWidth / aspect))
+        }
+      }
+
+      // margin, dimensions
+      const margin = { top: 30, right: 30, bottom: 180, left: 60 },
+        width = 325 - margin.left - margin.right,
+        height = 375 - margin.top - margin.bottom
+
+      // initialize graph
+      const svg = d3
         .select(ref.current)
         .append('svg')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
+        .call(responsiveFn)
         .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-      var x = d3
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+      // x-scale
+      const x = d3
         .scaleBand()
         .range([0, width])
         .domain(
@@ -27,18 +52,21 @@ const Graph = React.memo(({ stats }) => {
             return d.name
           })
         )
-        .padding(0.2)
+        .padding(0.1)
+
+      // y-scale
+      const y = d3.scaleLinear().domain([0, 100]).range([height, 0])
+
+      // append y/x axis
+      svg.append('g').call(d3.axisLeft(y))
+
       svg
         .append('g')
-        .attr('transform', 'translate(0,' + height + ')')
+        .attr('transform', `translate(0, ${height})`)
         .call(d3.axisBottom(x))
         .selectAll('text')
-        .attr('transform', 'translate(-10,0)rotate(-45)')
+        .attr('transform', 'translate(-10,0)rotate(-50)')
         .style('text-anchor', 'end')
-
-      // Add Y axis
-      var y = d3.scaleLinear().domain([0, 100]).range([height, 0])
-      svg.append('g').call(d3.axisLeft(y))
 
       const color = d3.scaleOrdinal([
         '#f83e26',
@@ -50,27 +78,19 @@ const Graph = React.memo(({ stats }) => {
       ])
 
       svg
-        .selectAll('mybar')
+        .selectAll('rect')
         .data(stats)
         .enter()
         .append('rect')
-        .attr('x', function (d) {
-          return x(d.name)
-        })
-        .attr('y', function (d) {
-          return y(d.base_stat)
-        })
+        .attr('x', (d) => x(d.name))
+        .attr('y', (d) => y(d.base_stat))
         .attr('width', x.bandwidth())
-        .attr('height', function (d) {
-          return height - y(d.base_stat)
-        })
-        .attr('fill', function (d, i) {
-          return color(i)
-        })
+        .attr('height', (d) => height - y(d.base_stat))
+        .attr('fill', (d, i) => color(i))
     }
   }, [stats])
 
-  return <div className={`${styles.graph}`} ref={ref}></div>
+  return <div id="graph" className={`${styles.graph}`} ref={ref}></div>
 })
 
 export default Graph
