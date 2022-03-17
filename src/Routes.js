478a700596556a3860b591e4e3f6ea17d5b5970a
gittenger <john.pittenger86@gmail.com>
2022-03-17 19:32:53 -0400

    scroll to type effect all routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,18 +4,25 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/General/Layout.component.jsx'
 import Home from './pages/Home.page.jsx'
 import HomeIndex from './pages/HomeIndex.page.jsx'
+import About from './pages/About.page.jsx'
 import PokemonDetails from './pages/PokemonDetails.page.jsx'
+import ScrollToTop from './utils/ScrollToTop'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomeIndex />} />
-          <Route path=":currentPage" element={<Home title="Home" />} />
-          <Route path="/pokemon/:pokemon" element={<PokemonDetails />} />
-        </Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomeIndex />} />
+            <Route path=":currentPage" element={<Home title="Home" />} />
+            <Route path="/pokemon/:pokemon" element={<PokemonDetails />} />
+          </Route>
+          <Route path="/about" element={<Layout />}>
+            <Route index element={<About />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   )
 }
