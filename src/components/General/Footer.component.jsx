ae83c0bda7f118255bd1183beb3ac110c23d7556
gittenger <john.pittenger86@gmail.com>
2022-03-18 12:17:10 -0400

    font for footer and author added

--- src/components/General/Footer.component.jsx
+++ src/components/General/Footer.component.jsx
@@ -9,35 +9,41 @@ const Footer = () => {
   } = images
 
   return (
-    <footer className="bg-zinc-1000 text-[dodgerblue] py-8 flex justify-between items-center">
-      <Link
-        className="ml-auto mr-10 underline hover:text-[midnightblue]"
-        to="/about"
-      >
-        About
-      </Link>
-      <ul
-        className={`${styles.iconsList} mr-auto flex justify-center items-center space-x-4`}
-      >
-        <li>
-          <a
-            href="https://github.com/Gittenger/pokeapi-2022"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GithubIcon />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://johnpittenger.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <WebIcon />
-          </a>
-        </li>
-      </ul>
+    <footer className="bg-zinc-1000 flex flex-col justify-center items-center py-8">
+      <div className="flex justify-between items-center text-[dodgerblue] mb-3">
+        <Link
+          className="ml-auto mr-10 font-card text-2xl underline hover:text-[midnightblue]"
+          to="/about"
+        >
+          About
+        </Link>
+        <ul
+          className={`${styles.iconsList} mr-auto flex justify-center items-center space-x-4`}
+        >
+          <li>
+            <a
+              href="https://github.com/Gittenger/pokeapi-2022"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubIcon />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://johnpittenger.dev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <WebIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex justify-center items-center text-white font-sansation">
+        <span className="mr-2">&copy;</span>
+        Website by John Pittenger
+      </div>
     </footer>
   )
 }
